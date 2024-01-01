d891715f0f6d820048ace3a9fc19997a4920b13b
sandeep-khande <sandeepkhande448@gmail.com>
2024-01-01 23:45:28 +0530

    added express

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ dotenv.config({
 })
 
 
-connectDb();
+connectDb()
+.then( () => {
+    app.listen(process.env.PORT || 5000)
+    console.log(`Server is running at ${process.env.PORT}`);
+})
+.catch( (err) => {
+    console.log("MONGODB connection failed !!", err);
+})
