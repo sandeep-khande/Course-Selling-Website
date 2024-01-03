81671b8a40bcc920b594bb18a21d8f26d665a8c0
sandeep-khande <sandeepkhande448@gmail.com>
2024-01-04 00:25:13 +0530

    added routes

--- src/app.js
+++ src/app.js
@@ -13,4 +13,12 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// importing routes
+
+import userRouter from './routes/user.routes.js'
+
+
+// routes declaration
+app.use("/users", userRouter)
+
 export { app }
